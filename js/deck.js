5ae26eb1b2df781bc2b4dd6f461fb1947670e5ad
jsjnbb5526vvs-coder <jsjnbb5526vvs@gmail.com>
2026-02-27 16:24:48 +0400

    Update deck.js

--- js/deck.js
+++ js/deck.js
@@ -1,29 +1,50 @@
 const Deck = {
 
-suits:["♥","♠","♦","♣"],
-values:["A","K","Q","J","10","9","8","7"],
+    suits:["♥","♠","♦","♣"],
+    values:["A","K","Q","J","10","9","8","7"],
 
-create(){
-let deck=[];
-this.suits.forEach(s=>{
-this.values.forEach((v,i)=>{
-deck.push({
-suit:s,
-value:v,
-power:8-i
-});
-});
-});
-deck.push({suit:"JOKER",value:"BIG",power:100});
-deck.push({suit:"JOKER",value:"SMALL",power:99});
-return deck;
-},
+    create(){
 
-shuffle(deck){
-for(let i=deck.length-1;i>0;i--){
-let j=Math.floor(Math.random()*(i+1));
-[deck[i],deck[j]]=[deck[j],deck[i]];
-}
-}
+        let deck = [];
+
+        this.suits.forEach(suit => {
+
+            this.values.forEach((value,index) => {
+
+                deck.push({
+                    suit: suit,
+                    value: value,
+                    power: 8 - index   // A أعلى = 8
+                });
+
+            });
+
+        });
+
+        // جوكر كبير
+        deck.push({
+            suit:"JOKER",
+            value:"BIG",
+            power:100
+        });
+
+        // جوكر صغير
+        deck.push({
+            suit:"JOKER",
+            value:"SMALL",
+            power:99
+        });
+
+        return deck;
+    },
+
+    shuffle(deck){
+
+        for(let i = deck.length - 1; i > 0; i--){
+            let j = Math.floor(Math.random() * (i + 1));
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+        }
+
+    }
 
 };
